9c576039a617f9b010973617f59d82f6fbb15d8b
Planarians <lyplanarians@gmail.com>
2024-02-17 23:59:10 +0000

    finish 101

---
+++ src/com/company/SymmetricTree101E.java
@@ -0,0 +1,66 @@
+package com.company;
+
+/**
+ * @program: Leetcode
+ * @description:
+ * @author: Mr.Wang
+ * @create: 2024-02-17 23:09
+ **/
+public class SymmetricTree101E {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null){
+            return false;
+        }
+        return dfs(root,root);
+    }
+
+    public Boolean dfs(TreeNode left,TreeNode right) {
+        if (left != null&&right!=null) {
+            if(left.val!=right.val){
+                return false;
+            }
+            if(!dfs(left.right,right.left)||!dfs(left.left,right.right)){
+                return false;
+            }
+        }else{
+            if(left==null&&right!=null){
+                return false;
+            }
+            if(right==null&&left!=null){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+/**
+ * 1
+ * 2   2
+ * 2 n 2
+ * <p>
+ * <p>
+ * /**
+ * }
+ */
