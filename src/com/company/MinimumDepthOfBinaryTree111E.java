a0c677f447d04d23305376662406cce1cb34c450
Planarians <lyplanarians@gmail.com>
2024-03-17 06:39:21 +0000

    finish 111E

---
+++ src/com/company/MinimumDepthOfBinaryTree111E.java
@@ -0,0 +1,61 @@
+package com.company;
+
+/**
+ * @program: Leetcode
+ * @description:
+ * @author: Mr.Wang
+ * @create: 2024-03-17 06:38
+ **/
+public class MinimumDepthOfBinaryTree111E {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    class Solution {
+        public int minDepth(TreeNode root) {
+            if(root==null){
+                return 0;
+            }
+            // if(root.left==null||root.right==null){
+            //     if(root.left==null){
+            //         return 1+dfs(root.right);
+            //     }else if(root.right==null){
+            //         return 1+dfs(root.left);
+            //     }else{
+            //         return 1;
+            //     }
+            // }
+            return dfs(root);
+        }
+        public int dfs(TreeNode root){
+            int left=0;
+            int right=0;
+            if(root!=null){
+                if(root.left==null&&root.right==null){
+                    return 1;
+                }
+                if(root.left==null){
+                    left=Integer.MAX_VALUE;
+                }else{
+                    left=dfs(root.left);
+                }
+                if(root.right==null){
+                    right=Integer.MAX_VALUE;
+                }else{
+                    right=dfs(root.right);
+                }
+                return 1+Math.min(left,right);
+            }else{
+                return 0;
+            }
+        }
+    }
+}
